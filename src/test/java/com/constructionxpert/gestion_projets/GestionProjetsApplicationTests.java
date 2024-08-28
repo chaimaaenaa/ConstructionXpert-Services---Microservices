6df3fe58de0697529be96cbf55fcb4fa1d5c2811
chaimaaenaa <mekayssichaimaaenaa@gmail.com>
2024-08-28 12:29:52 +0100

    create la configuration

---
+++ src/test/java/com/constructionxpert/gestion_projets/GestionProjetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.constructionxpert.gestion_projets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProjetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
